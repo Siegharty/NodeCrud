8d560651ba288267ac65614f4d16c370b8535f24
Kevin Cabrera <kcabrera@everis.com>
2020-09-06 16:50:08 -0300

    Instala dependencias Nodemon y dotenv

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const app = express();
 const address = '127.0.0.1';
 const port = process.env.PORT || 3000;
 
+app.get('/', (req, res, next) => {
+    res.send('Inicio');
+});
+
 app.listen(port, () => {
     console.log(`Servidor iniciado en http://${address}:${port}`);
 });
